c1ae67a4d9ac673a6632cc68f78fd51d181f7f68
Linus Tolke <linus@tigris.org>
2005-09-06 10:18:55 +0000

    Updates to get coverage correct for the Model subsystem.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@384 4ee4186a-551e-0410-be62-bbd4fc22c551

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x tools/apache-ant-1.6.1/bin/ant
-tools/apache-ant-1.6.1/bin/ant -lib tools/checkstyle-3.3 -lib tools/findbugs-0.7.4/lib -lib tools/jdepend-2.6/lib -lib tools/jcoverage-1.0.5/lib/junit/3.8.1 -lib tools/jcoverage-1.0.5 -lib tools/jcoverage-1.0.5/lib/bcel/5.1 -lib tools/jcoverage-1.0.5/lib/gnu/getopt/1.0.9 -lib tools/jcoverage-1.0.5/lib/log4j/1.2.8 -lib tools/jcoverage-1.0.5/lib/oro/2.0.7 "$@"
+tools/apache-ant-1.6.1/bin/ant -lib tools/checkstyle-3.3 -lib tools/findbugs-0.7.4/lib -lib tools/jdepend-2.6/lib -lib tools/jcoverage -lib tools/jcoverage-1.0.5/lib/junit/3.8.1 -lib tools/jcoverage-1.0.5 -lib tools/jcoverage-1.0.5/lib/bcel/5.1 -lib tools/jcoverage-1.0.5/lib/gnu/getopt/1.0.9 -lib tools/jcoverage-1.0.5/lib/log4j/1.2.8 -lib tools/jcoverage-1.0.5/lib/oro/2.0.7 "$@"
