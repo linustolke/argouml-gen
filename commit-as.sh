3f2f62708db4bd633d31fa3fc190229620eebe88
Linus Tolke <linus@tigris.org>
2006-05-13 19:35:57 +0000

    Moved the checked out copy of argouml-stats.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@405 4ee4186a-551e-0410-be62-bbd4fc22c551

--- commit-as.sh
+++ commit-as.sh
@@ -9,7 +9,7 @@
 #
 # It is assumed that you have the argouml-stats project checked out.
 # Check out with
-# svn co http://argouml-stats.tigris.org/svn/argouml-stats
+# svn co http://argouml-stats.tigris.org/svn/argouml-stats/trunk argouml-stats
 
 if test ! -d argouml-stats
 then
@@ -17,7 +17,7 @@ then
     exit 1
 fi
 
-if test "`(cd argouml-stats && svn info) | grep '^URL:'`" != "URL: http://argouml-stats.tigris.org/svn/argouml-stats"
+if test "`(cd argouml-stats && svn info) | grep '^URL:'`" != "URL: http://argouml-stats.tigris.org/svn/argouml-stats/trunk"
 then
     echo The argouml-stats directory is incorrectly downloaded.
     exit 1
@@ -27,8 +27,8 @@ fi
 
 for arg in $*
 do
-    cp -r tmp/argouml/build/$arg `dirname argouml-stats/trunk/www/$arg`
-    ( cd argouml-stats/trunk/www/$arg &&
+    cp -r tmp/argouml/build/$arg `dirname argouml-stats/www/$arg`
+    ( cd argouml-stats/www/$arg &&
       svn status | while read type path
       do
           case "$type" in
@@ -42,18 +42,18 @@ do
 done
 
 echo Rebuilding the index.html file.
-( cd argouml-stats/trunk/www && ls */*/index.html ) |
+( cd argouml-stats/www && ls */*/index.html ) |
 awk 'BEGIN { print "<html><head><title>ArgoUML Automatically Generated Files</title></head><body>";
              print "<ul>"; }
      { sub(/index.html/,"");
        printf "<li><a href=\"nonav/%s\">%s</a>\n", $NF, $NF; }
      END { print "</ul>";
-	   print "</body>"; }' > argouml-stats/trunk/www/index.html
+	   print "</body>"; }' > argouml-stats/www/index.html
 
 for arg in $*
 do
     echo Commiting $arg
-    ( cd argouml-stats/trunk/www/$arg &&
+    ( cd argouml-stats/www/$arg &&
       svn commit -m"New version of files in $arg."
     )
 done
