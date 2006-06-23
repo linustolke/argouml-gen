d914fb4a19b5b390e46b31aa2a041c8491656ae3
Linus Tolke <linus@tigris.org>
2006-06-23 21:07:21 +0000

    Reduce the complexity of the crontab files, moving it under cvs control.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@411 4ee4186a-551e-0410-be62-bbd4fc22c551

---
+++ perhost/closettop.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# $Id$
+
+# This is the script that is run by crontab on Linus' Linux host closettop
+
+if test ! -f build-reports.xml
+then
+    echo You need to start from the right directory.
+fi
+
+JAVA_HOME=/usr/local/lib/java/j2sdk1.4.2_10
+export JAVA_HOME
+
+./build.sh update
+
+( cd argouml-stats && svn update )
+
+./build.sh report:jcoverage &&
+  ./commit-as.sh reports/jcoverage reports/junit-result-jcoverage
+
+./build.sh report:jdepend &&
+  ./commit-as.sh reports/jdepend
+
+./build.sh report:checkstyle &&
+  ./commit-as.sh reports/checkstyle
+
+./build.sh report:javadocs &&
+  ./commit-as.sh reports/javadocs reports/javadocs-api
+
+./build.sh report:findbugs &&
+  ./commit-as.sh reports/findbugs
+
+./build.sh report:i18ncomparison &&
+  ./commit-as.sh reports/i18ncomparison
+
+
+./build.sh update-documentation
+
+./build.sh report:documentation &&
+  ./commit-as.sh documentation/defaulthtml
