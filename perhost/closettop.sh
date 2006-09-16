08e77bc6c53948752fc61dd5d6aeaef6c9884c54
Linus Tolke <linus@tigris.org>
2006-09-16 06:15:45 +0000

    Use the ant logfile argument instead. Broken long lines.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@423 4ee4186a-551e-0410-be62-bbd4fc22c551

--- perhost/closettop.sh
+++ perhost/closettop.sh
@@ -19,36 +19,45 @@ PRESENTED=argouml-stats/www/reports
 
 statusfile=$PRESENTED/jcoverage/status.txt
 rm $statusfile
-if ./build.sh report:jcoverage 2>&1 > $PRESENTED/jcoverage/output.txt
+if ./build.sh report:jcoverage -l $PRESENTED/jcoverage/output.txt
 then
   ./copy-add.sh reports reports/jcoverage reports/junit-result-jcoverage
   echo Succeeded at `date +"%b %d %H:%M"` > $statusfile
 else
   echo Failed at `date +"%b %d %H:%M"` > $statusfile
 fi
-( cd argouml-stats/www/reports && time svn commit -m'Commiting result from report:jcoverage' )
+(
+  cd argouml-stats/www/reports &&
+  time svn commit -m'Commiting result from report:jcoverage'
+)
 
 statusfile=$PRESENTED/jdepend/status.txt
 rm $statusfile
-if ./build.sh report:jdepend 2>&1 > $PRESENTED/jdepend/output.txt
+if ./build.sh report:jdepend -l $PRESENTED/jdepend/output.txt
 then
   ./copy-add.sh reports reports/jdepend
   echo Succeeded at `date +"%b %d %H:%M"` > $statusfile
 else
   echo Failed at `date +"%b %d %H:%M"` > $statusfile
 fi
-( cd argouml-stats/www/reports && time svn commit -m'Commiting result from report:jdepend' )
+(
+  cd argouml-stats/www/reports &&
+  time svn commit -m'Commiting result from report:jdepend'
+)
 
 statusfile=$PRESENTED/javadocs/status.txt
 rm $statusfile
-if ./build.sh report:javadocs > $PRESENTED/javadocs/output.txt
+if ./build.sh report:javadocs -l $PRESENTED/javadocs/output.txt
 then
   ./copy-add.sh reports reports/javadocs reports/javadocs-api
   echo Succeeded at `date +"%b %d %H:%M"` > $statusfile
 else
   echo Failed at `date +"%b %d %H:%M"` > $statusfile
 fi
-( cd argouml-stats/www/reports && time svn commit -m'Commiting result from report:javadocs' )
+(
+  cd argouml-stats/www/reports &&
+  time svn commit -m'Commiting result from report:javadocs'
+)
 
 ./build.sh report:checkstyle &&
   ./copy-add.sh reports reports/checkstyle
@@ -58,16 +67,25 @@ fi
 
 ./build.sh report:i18ncomparison &&
   ./copy-add.sh reports reports/i18ncomparison
-( cd argouml-stats/www/reports && time svn commit -m'Commiting result from report:checkstyle, report:findbugs, and 
-report:i18ncomparison' )
+(
+  cd argouml-stats/www/reports &&
+  time svn commit -m'Commiting result from report:checkstyle, report:findbugs, and 
+report:i18ncomparison'
+)
 
 ./build.sh update-documentation || exit 1
 
-if ./build.sh report:documentation 2>&1 > argouml-stats/www/documentation/output.txt
+if ./build.sh report:documentation -l argouml-stats/www/documentation/output.txt
 then
   ./copy-add.sh documentation documentation/defaulthtml documentation/printablehtml documentation/pdf
 fi
-( cd argouml-stats/www/documentation && time svn commit -m'Commiting result from report:documentation' )
+(
+  cd argouml-stats/www/documentation &&
+  time svn commit -m'Commiting result from report:documentation'
+)
 
 ./create-index.sh > argouml-stats/www/index.html
-( cd argouml-stats/www && time svn commit -m'Commiting all the rest.' )
+(
+  cd argouml-stats/www &&
+  time svn commit -m'Commiting all the rest.'
+)
