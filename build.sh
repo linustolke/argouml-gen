ae14a022b8d86145f56e17b2d3f994e4602fb0fe
Linus Tolke <linus@tigris.org>
2007-05-13 08:06:09 +0000

    More changes to get the Cobertura setup to work.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@466 4ee4186a-551e-0410-be62-bbd4fc22c551

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-tools/apache-ant-1.6.5/bin/ant -lib tools/checkstyle-4.2 -lib tools/findbugs-0.7.4/lib -lib tools/jdepend-2.6/lib -lib tools/jcoverage -lib tools/jcoverage-1.0.5/lib/junit/3.8.1 -lib tools/jcoverage-1.0.5 -lib tools/jcoverage-1.0.5/lib/bcel/5.1 -lib tools/jcoverage-1.0.5/lib/gnu/getopt/1.0.9 -lib tools/jcoverage-1.0.5/lib/log4j/1.2.8 -lib tools/jcoverage-1.0.5/lib/oro/2.0.7 "$@"
+
+# TODO: The Cobertura setup depends on things from the JCoverage setup.
+JCOVERAGE_ADDS="-lib tools/jcoverage -lib tools/jcoverage-1.0.5/lib/junit/3.8.1 -lib tools/jcoverage-1.0.5 -lib tools/jcoverage-1.0.5/lib/bcel/5.1 -lib tools/jcoverage-1.0.5/lib/gnu/getopt/1.0.9 -lib tools/jcoverage-1.0.5/lib/log4j/1.2.8 -lib tools/jcoverage-1.0.5/lib/oro/2.0.7"
+COBERTURA_ADDS="-lib tools/cobertura-1.8 -lib tools/cobertura-1.8/lib"
+
+tools/apache-ant-1.6.5/bin/ant \
+    -lib tools/checkstyle-4.2 \
+    -lib tools/findbugs-0.7.4/lib \
+    -lib tools/jdepend-2.6/lib \
+    $COBERTURA_ADDS \
+    $JCOVERAGE_ADDS \
+    "$@"
