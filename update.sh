d5b644a3d023951a0d323cbe3b80b3d674521592
Linus Tolke <linus@tigris.org>
2008-05-04 13:32:45 +0000

    Fixed the build for the Spanish and German documentation.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@503 4ee4186a-551e-0410-be62-bbd4fc22c551

--- update.sh
+++ update.sh
@@ -2,13 +2,27 @@
 
 # This is a list of all project for which we do nightly things:
 PROJECTS="argouml \
+              argoeclipse \
+              argouml-andromda \
+              argouml-classfile \
               argouml-cpp \
+              argouml-csharp \
+              argouml-db \
+              argouml-idl \
+              argouml-java \
+              argouml-php \
+              argouml-python \
+              argouml-sql \
+              \
               argouml-de argouml-en-gb argouml-es \
               argouml-fr \
               argouml-i18n-zh argouml-it argouml-nb \
               argouml-pt argouml-pt-br \
               argouml-ro argouml-ru \
-	      argouml-sv"
+	      argouml-sv
+              \
+              argoprint \
+              argopdf"
 
 
 (
