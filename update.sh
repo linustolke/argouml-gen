5941237866bf7ba8c8665133946c8acb87162a3a
Linus Tolke <linus@tigris.org>
2009-10-05 21:44:09 +0000

    Added functions to work from a local mirror.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@557 4ee4186a-551e-0410-be62-bbd4fc22c551

--- update.sh
+++ update.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+set -- `getopt mis "$@"`
+MIRRORED=false
+INITIALIZE=false
+SYNCHRONIZE=false
+for o
+do
+  case $o in
+  -m)
+    MIRRORED=true
+    ;;
+  -i)
+    INITIALIZE=true
+    ;;
+  -s)
+    SYNCHRONIZE=true
+    ;;
+  --)
+    ;;
+  *)
+    echo $0: Invalid argument $o 1>&2
+    ;;
+  esac
+done
+
 # This is a list of all project for which we do nightly things:
 PROJECTS="argouml \
               argouml-andromda \
@@ -24,6 +48,50 @@ PROJECTS="argouml \
               argopdf"
 
 
+ROOT=http:/
+export ROOT
+
+if $MIRRORED
+then
+  ROOT=file://$HOME/REPOS
+  export ROOT
+
+  for proj in $PROJECTS
+  do
+    MIDDLE=$proj.tigris.org/svn
+    PROJMIDDLE=$MIDDLE/$proj
+    DIR=$HOME/REPOS/$MIDDLE
+
+    if $INITIALIZE
+    then
+      echo creating mirror for $proj...
+      mkdir -p $DIR
+      (
+        cd $DIR
+        svnadmin create $proj
+        PRERPC=$proj/hooks/pre-revprop-change
+        echo "#!/bin/sh" > $PRERPC
+        echo exit 0 >> $PRERPC
+        chmod +x $PRERPC
+      )
+      svnsync initialize $ROOT/$PROJMIDDLE http://$PROJMIDDLE --username guest --password ""
+      echo creating mirror for $proj...done
+    fi
+
+    if $SYNCHRONIZE
+    then
+      echo synchronize $proj...
+      svnsync synchronize $ROOT/$PROJMIDDLE
+      echo synchronize $proj...done
+    fi
+  done
+
+  if $INITIALIZE || $SYNCHRONIZE
+  then
+    exit 0;
+  fi
+fi
+
 (
   if [ ! -d tmp ]
   then
@@ -33,6 +101,6 @@ PROJECTS="argouml \
   for proj in $PROJECTS
   do
     echo checking out $proj
-    svn co http://$proj.tigris.org/svn/$proj/trunk $proj --username=guest --password=''
+    svn co $ROOT/$proj.tigris.org/svn/$proj/trunk $proj --username=guest --password=''
   done
 )
