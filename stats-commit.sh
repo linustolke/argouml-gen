7f4d1941f8450103e47ad9e431ee5128600f02f4
Linus Tolke <linus@tigris.org>
2009-10-11 17:19:51 +0000

    Created the command for committing the contents of argouml-stats.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@559 4ee4186a-551e-0410-be62-bbd4fc22c551

---
+++ stats-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# $Id: closettop.sh 558 2009-10-08 21:38:24Z linus $
+
+# This script commits things in the argouml-stats project if the ordinary
+# commit failed.
+#
+# The argument, an integer, is the chunk size. 
+# If missing, all files are attempted.
+
+ARG=$1
+if test -z "$ARG"
+then
+  echo $(date): Committing all the rest...
+  cd argouml-stats/www
+  svn commit -m"Committing all the rest"
+  echo $(date): Committing all the rest...done
+else
+  echo $(date): Committing all the rest in chunks of $ARG files...
+  cd argouml-stats/www
+  svn update
+  svn status |
+  awk '/^[AM]/ { print $2; }' |
+  xargs -L $ARG --no-run-if-empty \
+      svn commit -m"Committing all the rest (in chunks of $ARG files)"
+  echo $(date): Committing all the rest in chunks of $ARG files...done
+fi
