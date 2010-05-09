c2ad9040329b7584e57e4e51b4953e087645ec31
Linus Tolke <linus@tigris.org>
2010-05-09 04:34:56 +0000

    Add synchronizing of argouml-stats.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@649 4ee4186a-551e-0410-be62-bbd4fc22c551

--- hudson/update-mirror2.sh
+++ hudson/update-mirror2.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 svnsync --non-interactive --no-auth-cache synchronize file:///home2/argonightly/REPOS/argoeclipse.tigris.org/svn/argoeclipse --username guest --password ""
+svnsync --non-interactive --no-auth-cache synchronize file:///home2/argonightly/REPOS/argoeclipse.tigris.org/svn/argouml-stats --username guest --password ""
