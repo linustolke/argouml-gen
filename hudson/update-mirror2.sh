e4ef47d65ca2a945cc86d4988d382befbdb8d8ea
Linus Tolke <linus@tigris.org>
2010-05-09 05:58:20 +0000

    Corrected the path to the argouml-stats mirror.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@653 4ee4186a-551e-0410-be62-bbd4fc22c551

--- hudson/update-mirror2.sh
+++ hudson/update-mirror2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 svnsync --non-interactive --no-auth-cache synchronize file:///home2/argonightly/REPOS/argoeclipse.tigris.org/svn/argoeclipse --username guest --password ""
-svnsync --non-interactive --no-auth-cache synchronize file:///home2/argonightly/REPOS/argoeclipse.tigris.org/svn/argouml-stats --username guest --password ""
+svnsync --non-interactive --no-auth-cache synchronize file:///home2/argonightly/REPOS/argouml-stats.tigris.org/svn/argouml-stats --username guest --password ""
