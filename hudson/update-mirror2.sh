52e490b0e4db8085a4282a38bfe26dfc95e596a1
Linus Tolke <linus@tigris.org>
2011-06-02 09:38:09 +0000

    Disconnected the script from a certain home directory.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@699 4ee4186a-551e-0410-be62-bbd4fc22c551

--- hudson/update-mirror2.sh
+++ hudson/update-mirror2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-URLS="file:///home2/argonightly/REPOS/argoeclipse.tigris.org/svn/argoeclipse \
-      file:///home2/argonightly/REPOS/argouml-stats.tigris.org/svn/argouml-stats"
+URLS="file://$HOME/REPOS/argoeclipse.tigris.org/svn/argoeclipse \
+      file://$HOME/REPOS/argouml-stats.tigris.org/svn/argouml-stats"
 for url in $URLS
 do
   svnsync --non-interactive --no-auth-cache synchronize $url \
