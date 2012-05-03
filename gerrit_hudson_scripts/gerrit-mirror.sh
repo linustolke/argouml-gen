fff22306097a94fa7811bc2e52b3ea8d1c51981b
Linus Tolke <linus@tigris.org>
2012-05-03 20:29:56 +0000

    Added the script to synchronize for gerrit.
    git-svn-id: http://argouml-gen.tigris.org/svn/argouml-gen/trunk@715 4ee4186a-551e-0410-be62-bbd4fc22c551

---
+++ gerrit_hudson_scripts/gerrit-mirror.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# Script to mirror between gerrit and the argouml subversion repositories.
+#
+# Assumptions:
+# Runs from within Jenkins
+# Submits to subversion are done with the given user id (SVN_USERNAME)
+# 
+SVN_USER=closettop_nightlybuild
+
+# The list of projects to include
+# TODO: This is sofar only some of the "small" projects.
+PROJECTS=" \
+              argouml-andromda \
+              argouml-actionscript3 \
+              argouml-ada \
+              argouml-atl \
+              argouml-cpp \
+              argouml-csharp \
+              argouml-db \
+              argouml-delphi \
+              argouml-graphviz \
+              argouml-idl \
+              argouml-java \
+              argouml-javascript \
+              argouml-pattern-wizard \
+              argouml-php \
+              argouml-python \
+              argouml-ruby \
+              argouml-sql \
+              \
+              argouml-ca \
+              argouml-de argouml-en-gb argouml-es \
+              argouml-fr \
+              argouml-hi \
+              argouml-i18n-zh argouml-it \
+              argouml-ja \
+              argouml-nb \
+              argouml-pt argouml-pt-br \
+              argouml-ro argouml-ru \
+	      argouml-sv \
+	      argouml-tr \
+         "
+
+set -- `getopt i "$@"`
+INITIALIZE=false
+while true
+do
+  case "$1" in
+  -i)
+    INITIALIZE=true
+    shift
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo $0: Invalid argument $o 1>&2
+    exit 1
+    ;;
+  esac
+done
+
+GITGERRITREPOS=$HOME/GITGERRITREPOS
+
+if $INITIALIZE
+then
+  if test ! -d $GITGERRITREPOS
+  then
+    mkdir $GITGERRITREPOS
+  fi
+  cd $GITGERRITREPOS
+  for proj in $PROJECTS
+  do
+    if test ! -d $proj
+    then
+      git svn init -s \
+	  http://$proj.tigris.org/svn/$proj $proj \
+	  --username=$SVN_USER
+      cd $proj
+      touch .git/git-daemon-export-ok
+      # Add gerrit as a remote
+      git remote add gerrit ssh://localhost:29418/$proj
+      git checkout -b trunk gerrit/trunk
+    fi
+  done
+fi
+
+# Update and upload
+cd $GITGERRITREPOS
+for proj in $PROJECTS
+do
+  (
+    cd $proj
+    git checkout gerrit/master
+    git fetch gerrit
+    git svn fetch --username=$SVN_USER
+    git rebase remotes/trunk
+    git svn dcommit --username=$SVN_USER
+  )
+done
+
