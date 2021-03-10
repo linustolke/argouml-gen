c633c2fa935d4ed9ae204d4c9d7c63c44574699c
Linus Tolke <ltolke@gmail.com>
2021-03-10 18:15:10 +0100

    More work to get it working.

--- setup/vbox/01_install.sh
+++ setup/vbox/01_install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -xe
 
-sudo apt install virtualbox-??
-sudo apt install vagrant
-sudo apt install ansible
+sudo apt install virtualbox vagrant ansible
 vagrant plugin install vagrant-disksize
