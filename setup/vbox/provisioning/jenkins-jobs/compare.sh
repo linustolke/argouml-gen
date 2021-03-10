62921b3e0224e45ff142d1ca6181650d32019746
Linus Tolke <ltolke@gmail.com>
2021-03-10 18:15:10 +0100

    More work to get it working.

--- setup/vbox/provisioning/jenkins-jobs/compare.sh
+++ setup/vbox/provisioning/jenkins-jobs/compare.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo diff -ru /vagrant/provisioning/jenkins-jobs /var/lib/jenkins/jobs
+sudo diff -ru \
+     --exclude=branches \
+     --exclude=compare-github-gerrithub \
+     --exclude=computation \
+     --exclude=indexing \
+     --exclude=jobs \
+     --exclude=compare.sh \
+     --exclude=config.xml.~*~ \
+     --exclude=jobs.yml \
+     --exclude=state.xml \
+     /vagrant/provisioning/jenkins-jobs /var/lib/jenkins/jobs
