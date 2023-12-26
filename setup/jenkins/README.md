cf10f5c14fa7e1ce852f853d7c78c0889762ace4
Linus Tolke <ltolke@gmail.com>
2023-12-26 11:15:13 +0100

    First version of script to bootstrap

--- setup/jenkins/README.md
+++ setup/jenkins/README.md
@@ -1,3 +1,12 @@
 # How to configure Jenkins
 
 If there are details that does not fit into the jobs-configurion.
+
+## Run jenkins
+
+Docker command
+```
+  docker run -p 8080:8080 -p 50000:50000 --restart=on-failure \
+      -v jenkins_home:/var/jenkins_home \
+      jenkins/jenkins:lts-jdk17
+```
---
+++ setup/jenkins/script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Script to start and stop the jenkins container(s)
+# This is normally run only to bootstrap the server
+extraargsbuild=
+extraargsrun=
+case "$1" in
+    update)
+        docker stop jenkins
+        docker stop jenkins-docker
+        docker rm jenkins
+        extraargsbuild="--pull"
+        extraargsrun="--pull=always"
+        ;&
+    start)
+        docker build $extraargsbuild . -t myjenkins:lts-jdk17
+        docker run \
+               --name jenkins-docker \
+               $extraargsrun \
+               --rm \
+               --detach \
+               --privileged \
+               --network jenkins \
+               --network-alias docker \
+               --env DOCKER_TLS_CERTDIR=/certs \
+               --volume jenkins-docker-certs:/certs/client \
+               --volume jenkins-data:/var/jenkins_home \
+               --publish 2376:2376 \
+               docker:dind \
+               --storage-driver overlay2
+        docker run \
+               --name jenkins \
+               --restart=on-failure \
+               --detach \
+               --network jenkins \
+               --network-alias jenkins \
+               --env DOCKER_HOST=tcp://docker:2376 \
+               --env DOCKER_CERT_PATH=/certs/client \
+               --env DOCKER_TLS_VERIFY=1 \
+               --publish 8080:8080 \
+               --publish 50000:50000 \
+               --volume jenkins-data:/var/jenkins_home \
+               --volume jenkins-docker-certs:/certs/client:ro \
+               myjenkins:lts-jdk17
+        ;;
+    stop)
+        docker stop jenkins
+        docker stop jenkins-docker
+        ;;
+    *)
+        echo "Incorrect parameter $1" 1>&2
+        exit 1;
+    ;;
+esac
